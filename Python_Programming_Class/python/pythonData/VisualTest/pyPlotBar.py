bcb4cc7fc48028429301db51c722b84d3ba4a9b1
songhyunsik <cutaandcrazy@gmail.com>
2024-05-28 17:25:35 +0900

    주피터 설치 / 시본의 기본 / 시본에서 사용하는 그래프 - 회귀 그래프, 산점도, 비교 그래프, 막대 그래프 실습

---
+++ Python_Programming_Class/python/pythonData/VisualTest/pyPlotBar.py
@@ -0,0 +1,29 @@
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.lines import lineStyles
+from matplotlib.patches import Shadow
+
+
+def main():
+    data = [[5., 25., 50., 20.], [4., 23., 51., 17.], [6., 22., 52., 19.]]
+
+    # 바 그래프
+    #x = np.arange(0, 8, 2)
+
+    # plt.bar(x + 0.00, data[0], color = 'b', width = 0.50)
+    # plt.bar(x + 0.50, data[1], color = 'g', width = 0.50)
+    # plt.bar(x + 1.0, data[2], color = 'r', width = 0.50)
+    # plt.xticks(X+0.50, ("A","B","C","D"))
+    data2 = np.array(data)
+    # 중복 허용 안함
+    color_list = np.random.choice(['b', 'g', 'r', 'y', 'm', 'c', 'k'], data2.shape[0])
+    data_label = list("ABCDEFGHIJKLMNOPQRSTUVWXYZ")[:data2.shape[0]]
+    X = np.arange(data2.shape[1])
+
+    for i in range(data2.shape[0]):
+        plt.bar(X, data2[1], bottom = np.sum(data2[:i], axis = 0), color = color_list[i], label = data_label[i])
+    plt.legend()
+    plt.show()
+
+if __name__ == "__main__":
+    main()
