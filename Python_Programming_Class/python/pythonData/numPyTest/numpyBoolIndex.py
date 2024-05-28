a267506075b2805ab80751136700293d6a6053d8
songhyunsik <cutaandcrazy@gmail.com>
2024-05-28 09:49:20 +0900

    numpyBoolIndex.py : all, any, where, argsort, argmax, argmin 실습 예제 numpyFancyIndex.py: boolean index, fancy index, example fancy index 실습 예제

--- Python_Programming_Class/python/pythonData/numPyTest/numpyBoolIndex.py
+++ Python_Programming_Class/python/pythonData/numPyTest/numpyBoolIndex.py
@@ -9,14 +9,27 @@ def main():
     print(f"x_1 : {x_1}")
     print(f"x_2 : {x_2}")
     print(f"x_1 > x_2 : {x_1 > x_2}")
+
+    # all any
     print(f"x_1 > x_2).all() : {(x_1 > x_2).all()}")
     print(f"x_1 > 100).any() : {(x_1 > 100).any()}")
     print(f"(x_1 > x_2).any() : {(x_1 > x_2).any()}")
     print(f"(x_1 > 0) : {(x_1 > 0)}")
     print(f"(x_1 > 0).all() : {(x_1 > 0).all()}")
-    
-    
 
+    # where
+    x = np.array([4, 6, 7, 3, 2])
+    print(f"x > 5 : {x > 5}")
+    print(f"np.where(x > 5) : {np.where(x > 5)}")
+    for i in np.where(x > 5):
+        print(f"x[i] : {x[i]}")
+
+    # argsort, argmax, argmin
+    print(f"np.argsort(x) : {np.argsort(x)}")
+    for i in np.argsort(x):
+        print(f"x[i] : {x[i]}")
+    print(f"np.argmax(x) : {np.argmax(x)}, {x[np.argmax(x)]}")
+    print(f"np.argmin(x) : {np.argmin(x)}, {x[np.argmin(x)]}")
 
 if __name__=="__main__":
     main()
