d51a9eec04cc143c6d1e22367d23b9446404806a
songhyunsik <cutaandcrazy@gmail.com>
2024-05-31 16:07:41 +0900

    챕터 4장 회귀 마무리 + 실습 예제들

---
+++ Python_Programming_Class/python/pythonData/partialResidualPlot.py
@@ -0,0 +1,28 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import statsmodels.api as sm
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score
+from statsmodels.stats.outliers_influence import OLSInfluence
+
+def main():
+    folder = "/home/songhyunsik/kuiotbigdata/Python_Programming_Class/python/pythonData/data/"
+    house : pd.DataFrame = pd.read_csv(folder + "house_sales.csv", sep='\t')
+    print(house.head())
+    print(house.info())
+    house_98105 = house.loc[house['ZipCode'] == 98105, :]
+    predictors = ['SqFtTotLiving', 'SqFtLot', 'Bathrooms', 'Bedrooms', 'BldgGrade']
+    outcome = 'AdjSalePrice'
+    print(house.groupby('ZipCode')['AdjSalePrice'].mean())
+
+    house_outlier = sm.OLS(house_98105[outcome], house_98105[predictors].assign(const=1))
+    results_98105 = house_outlier.fit()
+    influence = OLSInfluence(results_98105)
+    fig = plt.figure()
+    ax = fig.add_subplot()
+    sm.graphics.plot_ccpr(results_98105, 'SqFtTotLiving', ax=ax)
+    plt.show()
+    
+if __name__ == "__main__":
+    main()
