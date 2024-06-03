bd46fef420ee0d0e256753006af8864f8d316122
songhyunsik <cutaandcrazy@gmail.com>
2024-06-03 16:21:18 +0900

    트리 모델 실습 예제

---
+++ Python_Programming_Class/python/pytorch/data/dataSetTest.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import torch
+from torch.utils.data import DataLoader, Dataset
+
+class MyDataset(Dataset):
+    def __init__(self, csv_file):
+        self.label = pd.read_csv(csv_file)
+    
+    def __len__(self):
+        return len(self.label)
+    
+    def __getitem__(self, idx):
+        sample = torch.tensor(self.label.iloc[idx, 0:3]).int()
+        label = torch.tensor(self.label.iloc[idx, 3]).int()
+        return sample, label
+
+def main():
+    folder = "/home/songhyunsik/kuiotbigdata/Python_Programming_Class/python/pytorch/data/"
+    tensor_dataset = MyDataset(folder +"covtype.csv")
+    dataset = DataLoader(tensor_dataset, batch_size=100, shuffle=True)
+    
+    for i, (sample, label) in enumerate(dataset):
+        print(f"sample : {sample}")
+        print(f"label : {label}")
+
+if __name__ == "__main__":
+    main()
---
+++ Python_Programming_Class/python/pytorch/data/train_value_split.py
@@ -0,0 +1,11 @@
+import numpy as np
+import pandas as pd
+
+
+def main():
+    folder = "/home/songhyunsik/kuiotbigdata/Python_Programming_Class/python/pytorch/data/"
+    tensor_dataset = MyDataset(folder +"covtype.csv")
+    
+
+if __name__ == "__main__":
+    main()
