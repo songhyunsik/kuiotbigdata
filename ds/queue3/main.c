123fb00e95e0d52494ad1467ffc02423ed762f60
songhyunsik <160800406+songhyunsik@users.noreply.github.com>
2024-06-19 01:41:19 +0000

    수정

--- ds/queue3/main.c
+++ ds/queue3/main.c
@@ -7,21 +7,21 @@ int main(void)
     
     push(&s1, 100);
     push(&s1, 200);
-
+    push(&s1, 300);
     printf("s1 1st pop() : %d\n", pop(&s1));
 
-    push(&s1, 300);
+    
 
     printf("s1 2nd pop() : %d\n", pop(&s1));
     printf("s1 3rd pop() : %d\n", pop(&s1));
 
-
     push(&s2, 700);
-    push(&s2, 800);
+    push(&s2, 800);    
+    push(&s2, 900);    
 
     printf("s2 1st pop() : %d\n", pop(&s2));
 
-    push(&s2, 900);
+    
 
     printf("s2 2nd pop() : %d\n", pop(&s2));
     printf("s2 3rd pop() : %d\n", pop(&s2));
